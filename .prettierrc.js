fea54bd6713054be7b3350035ae2c8dd813b318b
zhichaoSun <zhichao_sun@outlook.com>
2024-05-01 21:36:03 +0800

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto'
+}
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,13 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
+
+module.exports = {
+  mode: "development", 
+  devServer: {
+    hot: true,
+    open: true
+  },
+  devtool: "cheap-module-source-map",
+  plugins: [
+    new ReactRefreshWebpackPlugin()
+  ]
+}
